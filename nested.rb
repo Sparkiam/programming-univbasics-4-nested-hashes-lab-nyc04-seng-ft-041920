7e1ac2c41006b13e24a1dbc948d50ef82b235dbd
Dell Wilson <warren.wilsonjr89@gmail.com>
2020-04-30 15:17:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -141,7 +141,7 @@ def adding_to_dennis
     }
     
   
-    programmer_hash[:dennis_ritchie][:languages][1]
+    programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
     
     programmer_hash
     
